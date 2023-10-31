2f024c54d5a2164185c0a2614ca3ce01b4398807
naopeke <naopeke@protonmail.com>
2023-10-31 11:15:16 +0100

    repaso2 Distancias completed(modified comment)

--- algebraProject/pointTest.ts
+++ algebraProject/pointTest.ts
@@ -2,10 +2,14 @@ import { Point } from './point'
 
 let myPoint1 = new Point(5, 10);
 
+//método toString
 console.log(myPoint1.toString());
+
+//método distance to origin
 let distanceToOrigin = myPoint1.distanceTolOrigin();
 console.log(`distance to origin : ${distanceToOrigin}`);
 
+//método distance to another point
 let myPoint2 = new Point(13, 15);
 let distanceToAnotherPoint = myPoint1.calculateDistance(myPoint2);
 console.log(`distance to another point : ${distanceToAnotherPoint}`);
